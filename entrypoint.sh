7dab4f9a7da3ede7dda2284ce1f92097601c1679
Edwin Pers <pers.edwin@gmail.com>
2026-06-09 23:10:48 +0000

    source bashrc in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eu
 
+cd "${HOME}" || exit 1
+. $HOME/.bashrc
+
 if [[ -v HF_HUB_TOKEN ]]; then
   echo "Logging into huggingface..."
   hf auth login --token $HF_HUB_TOKEN --add-to-git-credential
