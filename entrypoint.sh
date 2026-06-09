4d6783499e4b589d754e37422e57ea83ddaab5d4
Edwin Pers <pers.edwin@gmail.com>
2026-06-09 23:13:21 +0000

    more fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ cd "${HOME}" || exit 1
 
 if [[ -v HF_HUB_TOKEN ]]; then
   echo "Logging into huggingface..."
+  git config --global credential.helper store
   hf auth login --token $HF_HUB_TOKEN --add-to-git-credential
 fi
 
